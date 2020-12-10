cae697cfca0c1b304df6919767655d855de142a2
Tino Rusch <tino.rusch@gmail.com>
2020-12-10 14:01:39 +0100

    remove examples

--- example/generated/model_denis_all_of.go
+++
@@ -1,9 +0,0 @@
-// This file is auto-generated, DO NOT EDIT.
-//
-// Source:
-//     Title: Test Service
-//     Version: 0.1.0
-package api
-
-// DenisAllOf is an object.
-type DenisAllOf map[string]interface{}
