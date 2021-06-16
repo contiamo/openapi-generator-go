e3d44e4bfd672a9852e3924cf855906b89da300b
Lucas Roesler <roesler.lucas@gmail.com>
2021-06-16 15:14:54 +0200

    fix: Allow responses section in components to be omitted
    The `components.responses` is an optional section of the API spec. This
    fix allows the section to be omitted in the source API spec.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- pkg/filters/openapi_test.go
+++ pkg/filters/openapi_test.go
@@ -14,25 +14,34 @@ func TestFilterByPath(t *testing.T) {
 
 	cases := []struct {
 		name    string
+		source  string
 		filters []string
 	}{
 		{
-			name: "empty filter list",
+			name:   "empty filter list",
+			source: "source.yaml",
 		},
 		{
 			name:    "filter with single path",
+			source:  "source.yaml",
 			filters: []string{"/api/v2/{projectId}/fields"},
 		},
 		{
 			name:    "filter with multiple paths",
+			source:  "source.yaml",
 			filters: []string{"/api/v2/{projectId}/fields", "/api/v2/{projectId}/field-sets/{resourceKind}"},
+		},
+		{
+			name:    "filter without responses component",
+			source:  "source_without_responses_component.yaml",
+			filters: []string{"/api/v2/{projectId}/fields"},
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 
-			sourceFile, err := os.Open(filepath.Join("testdata", "source.yaml"))
+			sourceFile, err := os.Open(filepath.Join("testdata", tc.source))
 			require.NoError(t, err)
 
 			out, err := ByPath(sourceFile, tc.filters)
@@ -42,7 +51,7 @@ func TestFilterByPath(t *testing.T) {
 			exp, err := ioutil.ReadFile(filepath.Join("testdata", testFolder, "out.yaml"))
 			require.NoError(t, err)
 
-			require.YAMLEq(t, string(exp), string(out))
+			require.YAMLEq(t, string(exp), string(out), string(out))
 		})
 	}
 }
