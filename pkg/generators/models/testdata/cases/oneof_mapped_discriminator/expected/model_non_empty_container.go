317299d3b2d7adb387c052224070fbe17be0ad84
Florian Sachse <florian@contiamo.com>
2022-12-06 17:14:00 +0100

    fix: panic on oneof validation (#86)
    * test for illustration
    
    * add error on empty oneoff
    
    * add required check for validation
    
    * use custom validator only for nilable case

---
+++ pkg/generators/models/testdata/cases/oneof_mapped_discriminator/expected/model_non_empty_container.go
@@ -0,0 +1,36 @@
+// This file is auto-generated, DO NOT EDIT.
+//
+// Source:
+//
+//	Title: Test oneOf discriminator support
+//	Version: 0.1.0
+package generatortest
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+// NonEmptyContainer is an object.
+type NonEmptyContainer struct {
+	// Error:
+	Error Error `json:"error"`
+}
+
+// Validate implements basic validation for this model
+func (m NonEmptyContainer) Validate() error {
+	return validation.Errors{
+		"error": validation.Validate(
+			m.Error, validation.NotNil,
+		),
+	}.Filter()
+}
+
+// GetError returns the Error property
+func (m NonEmptyContainer) GetError() Error {
+	return m.Error
+}
+
+// SetError sets the Error property
+func (m *NonEmptyContainer) SetError(val Error) {
+	m.Error = val
+}
